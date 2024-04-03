0a81a5e7bc9eb85547a31372cedefe856d1c5557
Sebaaa._.as <100049059+ITAD0RI-YUJI@users.noreply.github.com>
2024-04-03 07:54:17 -0500

    Agregando todo lo elaborado en clase

---
+++ Banda/src/co/com/itadori/banda/logic/Guitarra.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.com.itadori.banda.logic;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Guitarra extends Instrumento {
+
+    @Override
+    public String afinar() {
+        return "Afinando Guitarra";
+    }
+
+    @Override
+    public String tocar() {
+        return "Tocando Guitarra";
+    }
+    
+}
